8adddbada69f5d89694fa33e935ae039bb60d7bd
JuanF20 <stromae18001@gmail.com>
2023-02-01 23:11:45 -0500

    subir1

--- src/modules/ventas/dto/categories/base-category.dto.ts
+++ src/modules/ventas/dto/categories/base-category.dto.ts
@@ -1,4 +1,30 @@
-export class BaseCategoryDto{
+import { IsNotEmpty } from "class-validator";
+import { isPositive, IsString } from "class-validator/types/decorator/decorators";
+import { ProductEntity } from "../../entities/product.entity";
+// import { isNotEmptyValidationOpntions, IsStringValidationOpntions,IsPositiveValidationOpntions } from '@shared/validation'
+export  class BaseCategorytDto{
+
+    @IsNotEmpty(isNotEmptyValidationOpntions())
+    @IsString(IsStringValidationOpntions())
+    readonly name:string;
+
+
+    @IsNotEmpty(isNotEmptyValidationOpntions())
+    @IsString(IsStringValidationOpntions())
+    readonly product:ProductEntity[];
     
-    
+}
+
+function isNotEmptyValidationOpntions(): import("class-validator").ValidationOptions {
+    throw new Error("Function not implemented.");
+}
+
+
+function IsStringValidationOpntions(): import("class-validator").ValidationOptions {
+    throw new Error("Function not implemented.");
+}
+
+
+function IsPositiveValidationOpntions(): unknown {
+    throw new Error("Function not implemented.");
 }
