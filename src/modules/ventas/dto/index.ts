43d0b18c68b4b773e84cd3ebb83c9ceac396299c
JuanF20 <stromae18001@gmail.com>
2023-02-02 21:14:57 -0500

    subir4

--- src/modules/ventas/dto/index.ts
+++ src/modules/ventas/dto/index.ts
@@ -1,25 +1,9 @@
-//Products
-export * from './products/base-product.dto';
-export * from './products/create-product.dto';
-export * from './products/filter-product.dto';
-export * from './products/read-product.dto';
-export * from './products/update-product.dto';
-
-
-//Category
-export * from './categories/base-category.dto';
-export * from './categories/create-category.dto';
-export * from './categories/filter-category.dto';
-export * from './categories/read-category.dto';
-export * from './categories/update-category.dto';
-
 // Cocinero
 export * from './cocinero/base-cocinero.dto';
 export * from './cocinero/create-cocinero.dto';
 export * from './cocinero/filter-cocinero.dto';
 export * from './cocinero/read-cocinero.dto';
 export * from './cocinero/update-cocinero.dto';
-
 
 //Cocina
 export * from './cocina/base-cocina.dto';
@@ -28,5 +12,16 @@ export * from './cocina/filter-cocina.dto';
 export * from './cocina/read-cocina.dto';
 export * from './cocina/update-cocina.dto';
 
+//Products
+export * from './products/base-product.dto';
+export * from './products/create-product.dto';
+export * from './products/filter-product.dto';
+export * from './products/read-product.dto';
+export * from './products/update-product.dto';
 
-
+//Category
+export * from './categories/base-category.dto';
+export * from './categories/create-category.dto';
+export * from './categories/filter-category.dto';
+export * from './categories/read-category.dto';
+export * from './categories/update-category.dto';
---
+++ src/modules/ventas/entities/index.ts
@@ -0,0 +1,4 @@
+export * from './cocinero.entity';
+export * from './cocina.entity';
+export * from './product.entity';
+export * from './category.entity';
